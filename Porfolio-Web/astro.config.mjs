262a7d7b8bc027debfcf5e56f3c924370c9a3fb1
mateoparentini <parentinimateo@gmail.com>
2026-01-02 18:17:08 -0300

    funct(fix): Deploy en netlify

--- Porfolio-Web/astro.config.mjs
+++ Porfolio-Web/astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
-// https://astro.build/config
 export default defineConfig({
+  output: "static",   
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://porfolio.dev/',
 })
